a573149c024778f03f3427cbb04d15e6528113b5
shrekt444 <andrea.maie444@gmail.com>
2021-04-07 15:41:10 +0200

    test

--- js/script.js
+++ js/script.js
@@ -33,7 +33,6 @@ var timerId = setInterval(countdown, 1000);
 function countdown() {
     if (timeLeft == -1) {
     clearTimeout(timerId);
-    doSomething();
 
     } else {
     elem.innerHTML = timeLeft + ' seconds remaining';
